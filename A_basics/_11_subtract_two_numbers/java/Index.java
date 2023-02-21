3e19f69e49de4ad1763ecbebf8996080dfb0cb27
danielOuattara <daniel.ouattara@gmx.com>
2023-02-21 21:22:11 +0100

    subtract two  numbers

---
+++ A_basics/_11_subtract_two_numbers/java/Index.java
@@ -0,0 +1,10 @@
+// package _11_substract_two_numbers.java;
+
+// declare variables
+class Index {
+
+    public static void main(String[] args) {
+        final int MY_NUM = 10 - 5;
+        System.out.println(MY_NUM);
+    }
+}
