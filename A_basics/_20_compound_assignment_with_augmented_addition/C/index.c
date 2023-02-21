553ed02282e02b9761bb9d0e5eb36067a3ea1d22
danielOuattara <daniel.ouattara@gmx.com>
2023-02-21 21:25:45 +0100

    compound assignment with augmented addition

---
+++ A_basics/_20_compound_assignment_with_augmented_addition/C/index.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello C\n");
+
+    short int a = 3;
+    short int b = 17;
+    float c = 12.54f;
+
+    // Only change code below this line
+    a += 12;
+    b += 9;
+    c += 7;
+
+    printf("%hd \n", a);
+    printf("%hd \n", b);
+    printf("%f \n", c);
+
+    return 0;
+}
---
+++ A_basics/_20_compound_assignment_with_augmented_addition/java/Index.java
@@ -0,0 +1,21 @@
+// package _20_compound_assignment_with_augmented_addition.java;
+
+// declare variables
+class Index {
+
+    public static void main(String[] args) {
+        byte a = 3;
+        byte b = 17;
+        float c = 12.54f;
+
+        // Only change code below this line
+        a += 12;
+        b += 9;
+        c += 7;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
