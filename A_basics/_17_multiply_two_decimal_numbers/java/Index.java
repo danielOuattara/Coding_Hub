5a03d5a6de413c5cc783a342ad900b5e48c5e115
danielOuattara <daniel.ouattara@gmx.com>
2023-02-21 21:24:31 +0100

    multiply two decimal numbers

---
+++ A_basics/_17_multiply_two_decimal_numbers/java/Index.java
@@ -0,0 +1,12 @@
+// package _17_multiply_two_decimal_numbers.java;
+
+// declare variables
+class Index {
+
+    public static void main(String[] args) {
+
+        final float MY_NUMBER = 34.5f * 1.5f;
+        System.out.println(MY_NUMBER);
+
+    }
+}
---
+++ A_basics/_17_multiply_two_decimal_numbers/php/index.php
@@ -0,0 +1,10 @@
+
+<?php
+
+const MY_NUMBER = 34.5 * 1.5;
+echo MY_NUMBER . "\n";
+
+
+
+
+// run in terminal: php index.php
