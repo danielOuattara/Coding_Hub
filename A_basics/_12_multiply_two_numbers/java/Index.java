5a7126a2700606627fa97ea3c5be06413170d39f
danielOuattara <daniel.ouattara@gmx.com>
2023-02-21 21:22:24 +0100

    multiply two numbers

---
+++ A_basics/_12_multiply_two_numbers/java/Index.java
@@ -0,0 +1,10 @@
+package _12_multiply_two_numbers.java;
+
+// declare variables
+class Index {
+
+    public static void main(String[] args) {
+        final int MY_NUM = 10 * 5;
+        System.out.println(MY_NUM);
+    }
+}
---
+++ A_basics/_12_multiply_two_numbers/javascript/index.js
@@ -0,0 +1,2 @@
+const MY_NUM = 10 * 5;
+console.log(MY_NUM);
